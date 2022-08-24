ce0a01bd5b85c29e551f1822a60d6587cc4bc283
Flaviohmm <fhenrique609@gmail.com>
2022-08-24 11:53:03 -0300

    Configurando CORS na API para requests de outras origens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'members-frontend';
+
+  members = [
+    {name: 'Member 01', id: 1, surname: "Ciclano", photo: 'http://www.minhaapp.com/photo1'},
+    {name: 'Member 02', id: 2, surname: "Beltrano", photo: 'http://www.minhaapp.com/photo2'},
+    {name: 'Member 03', id: 3, surname: "Fulano", photo: 'http://www.minhaapp.com/photo3'}
+  ];
+
+  constructor(private api: ApiService) {
+    this.getMembers();
+  }
+
+  getMembers = () => {
+    this.api.getAllMembers().subscribe(
+      data => {
+        this.members = data
+      },
+      error => {
+        console.log("Aconteceu um erro", error.message);
+      }
+    );
+  }
 }
